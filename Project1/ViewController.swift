e16fbe4dd87e51fe8a774b312102ad8e0dd1aad3
srrmstk <sorrymistake@outlook.com>
2023-05-22 21:29:22 +0700

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by srrmstk on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
